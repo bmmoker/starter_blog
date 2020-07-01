680180d24df6a718915f131142c29f9fdd3b458e
bryce <gitkraken@brycemoker.com>
2020-07-01 17:34:01 -0400

    removing import layout.css into layout.js, moved to typography

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
 import { ThemeToggler } from "gatsby-plugin-dark-mode"
-import "./layout.css"
 
 import { rhythm, scale } from "../utils/typography"
 
